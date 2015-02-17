f438eaa305d0347d49438640060fcfd38ae7fbbb
m_matsubara <m_matsubara@okwave.co.jp>
2015-02-17 20:10:33 +0900

    typus４向けの設定ファイルを埋め込み

--- lib/typus_extention.rb
+++ lib/typus_extention.rb
@@ -1,15 +1,10 @@
 # encoding: utf-8
 #
 module Typus
-  module I18n
-    def self.hoge
-    end
-    class<< self
-      alias :old_available_locales :available_locales
-      def available_locales
-        self.old_available_locales.merge( "日本語" => "ja", "ベトナム語" => "vi" )
-      end
+  class<< self
+    alias :old_available_locales :available_locales
+    def available_locales
+      self.old_available_locales.merge( "日本語" => "ja" )
     end
   end
 end
-
